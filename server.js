44400f3288402215d307a37f7660704029f0db5e
TrgPhan <23020443@vnu.edu.vn>
2025-02-20 23:48:35 +0700

    Add initial files and configurations for various node modules

--- server.js
+++ server.js
@@ -1,12 +1,29 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const db = require("better-sqlite3")("database.db"); // dùng sqlite3 để tạo database.db
+db.pragma("journal_mode = WAL");
+
+// tạo bảng users ở đây
+app.createTable = db.transaction(() => {
+    db.prepare(
+        'CREATE TABLE IF NOT EXISTS users(id INTEGER PRIMARY KEY AUTOINCREMENT, email TEXT, password TEXT)'
+    ).run();
+});
 
 app.set('view engine', 'ejs');
 app.set('views', './views');
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static("public"));
 
+app.use(function (req, res, next) {
+    res.locals.login_errors = [];
+    next();
+});
+app.use(function (req, res, next) {
+    res.locals.register_errors = [];
+    next();
+});
 app.get('/', (req, res) => {
     res.render('homepage', { title: 'Trang chủ' })
 })
@@ -15,8 +32,9 @@ app.get('/login', (req, res) => {
     res.render('login', { title: 'Đăng nhập' })
 })
 
-app.post('/register', (req, res) => {
-    const errors = [];
+app.post('/login', (req, res) => {
+    console.log(req.body);
+    const login_errors = [];
     if (typeof req.body.email !== 'string') {
         req.body.email = '';
     }
@@ -24,25 +42,41 @@ app.post('/register', (req, res) => {
         req.body.password = '';
     }
     req.body.email = req.body.email.trim();
-    req.body.password = req.body.password.trim();
-    if (!req.body.email) {
-        errors.push('Email should not be empty');
+    if (req.body.password.length < 6) {
+        login_errors.push('Password is too short');
     }
-    if (!req.body.password) {
-        errors.push('Password should not be empty');
-    }
-    if (req.body.email && !req.body.email.includes('@')) {
-        errors.push('Email should have @ character');
-    }
-    if (req.body.email && !req.body.email.match(/^[a-zA-Z0-9]+$/)) {
-        errors.push('Email should have character');
-    }
-    if (errors.length) {
-        res.render('homepage', { title: 'Trang chủ', errors });
+    if (login_errors.length) {
+        res.render('login', { title: 'Login Error', login_errors });
         // So we can use the errors message
         return;
     } else {
-        res.send('Thank you for registering');
+        res.render('homepage', { title: 'Trang chủ' });
+    }
+})
+
+app.post('/register', (req, res) => {
+    // nên tối ưu vào 1 hàm riêng
+    console.log(req.body);
+    const register_errors = [];
+    if (typeof req.body.email !== 'string') {
+        req.body.email = '';
+    }
+    if (typeof req.body.password !== 'string') {
+        req.body.password = '';
+    }
+    req.body.email = req.body.email.trim();
+    if (req.body.password.length < 6) {
+        register_errors.push('Password is too short');
+    }
+    if (req.body.password_confirm != '' && req.body.password != req.body.password_confirm) {
+        register_errors.push('Password confirm is not match');
+    }
+    if (register_errors.length) {
+        res.render('signup', { title: 'Register Error', register_errors });
+        // So we can use the errors message
+        return;
+    } else {
+        res.render('homepage', { title: 'Trang chủ' });
     }
 })
 

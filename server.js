1d6a4db0f6ce28c577d5054da6c19549e393c5f1
TrgPhan <23020443@vnu.edu.vn>
2025-02-20 20:01:20 +0700

    Add registration functionality and update views for login and signup

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const app = express()
 const port = 3000
 
 app.set('view engine', 'ejs');
+app.set('views', './views');
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static("public"));
 
 app.get('/', (req, res) => {
@@ -11,6 +13,37 @@ app.get('/', (req, res) => {
 
 app.get('/login', (req, res) => {
     res.render('login', { title: 'Đăng nhập' })
+})
+
+app.post('/register', (req, res) => {
+    const errors = [];
+    if (typeof req.body.email !== 'string') {
+        req.body.email = '';
+    }
+    if (typeof req.body.password !== 'string') {
+        req.body.password = '';
+    }
+    req.body.email = req.body.email.trim();
+    req.body.password = req.body.password.trim();
+    if (!req.body.email) {
+        errors.push('Email should not be empty');
+    }
+    if (!req.body.password) {
+        errors.push('Password should not be empty');
+    }
+    if (req.body.email && !req.body.email.includes('@')) {
+        errors.push('Email should have @ character');
+    }
+    if (req.body.email && !req.body.email.match(/^[a-zA-Z0-9]+$/)) {
+        errors.push('Email should have character');
+    }
+    if (errors.length) {
+        res.render('homepage', { title: 'Trang chủ', errors });
+        // So we can use the errors message
+        return;
+    } else {
+        res.send('Thank you for registering');
+    }
 })
 
 app.get('/signup', (req, res) => {
